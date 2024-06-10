82f68e08da7f73ccc7904a8efdc1fbae3dbc7837
LingBu <xcmabu@outlook.com>
2024-06-10 17:07:20 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://apk.bot/";//your website url
+  let target = "https://google/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
